e5130ffa04e7c896159ca58957eee098754e6290
sachin.thakur <Sachin.Thakur@allocatesoftware.com>
2019-09-15 15:46:15 +0530

    new package added

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 var express = require('express');
+var npmRun = require('npm-run');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var output = null;
 
-app.get('/', function(request, response){
-    response.send("Hello World !!!");
+app.get('/',  async function(request, response){
+    var result = await runNpmShow();
+    response.send(result);
 })
 
 app.get('/message', function(request, response){
-    console.log(request.params.message)
-    var message = request.params.message;
-    response.send(request);
+    console.log(request.query.message)
+    var message = request.query.message;
+    response.send(message);
 })
 
 
 app.listen(PORT, function(){
     console.log('Server is Started at', PORT)
 })
+
+
+function runNpmShow() {
+
+      return new Promise((resolve) => {
+        npmRun.exec(`npm show redux versions`, (err, stdout) => {
+          if (!err) {
+            const depVersionsList = JSON.parse(stdout.replace(/'/g, '"'));
+            resolve(depVersionsList);
+          } else {
+            console.error(`Could not fetch version info for: Skip.`);
+            resolve(err);
+          }
+        });
+      });
+    
+  };
+
+
