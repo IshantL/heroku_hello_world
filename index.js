4fd259e31f6ade2abb8f3937a81ee627fa6cc40b
sachin.thakur <Sachin.Thakur@allocatesoftware.com>
2019-09-16 18:07:15 +0530

    react integration

--- index.js
+++ index.js
@@ -21,15 +21,6 @@ app.post('/api/world', (req, res) => {
 });
 
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
-    // Handle React routing, return all requests to React app
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
 
 app.get('/', async function (request, response) {
     var result = await runNpmShow();
